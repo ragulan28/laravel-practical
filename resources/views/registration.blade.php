7a05eaf738984ce68ff75cfc3db1860faa140ac7
Ragulan <Ragulan28@gmail.com>
2018-06-10 15:47:33 +0530

    error msg added at the field in regristation form

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -19,15 +19,19 @@
 
     <label for="first_name"> First name</label>
     <input type="text" name="first_name" id="first_name">
+    <span>{{$errors->first('first_name')}}</span>
     <br>
     <label for="last_name"> Last name</label>
     <input type="text" name="last_name" id="first_name">
+    <span>{{$errors->first('last_name')}}</span>
     <br>
     <label for="first_name"> Username</label>
     <input type="text" name="username" id="first_name">
+    <span>{{$errors->first('username')}}</span>
     <br>
     <label for="first_name"> Password</label>
     <input type="password" name="password" id="first_name">
+    <span>{{$errors->first('password')}}</span>
     <br>
     <input value="Submit" type="submit">
 </form>
